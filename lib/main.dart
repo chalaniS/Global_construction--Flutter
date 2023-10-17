7629c9b263f19ef2777f67d2f6b4921d6bbed516
chalaniS <chalasaumya@gmail.com>
2023-10-17 14:07:11 +0530

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'Supplier/pages/home_page.dart';
-import 'Supplier/widgets/shopping_cart.dart';
 import 'firebase_options.dart';
 
 import 'theme.dart';
