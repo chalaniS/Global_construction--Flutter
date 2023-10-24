254326bda4a466a339d46b0a345c88783b04d390
IT21378270 <it21378270@my.sliit.lk>
2023-10-24 23:53:07 +0530

    backend modifications

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WonderSL',
+      title: 'Global Constructions',
       debugShowCheckedModeBanner: false,
       theme: lightTheme(context),
       home: const RequisitionsHome(),
